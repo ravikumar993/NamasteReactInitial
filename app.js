39869a05eed14f4370c4c1c8ad535c5d614dc1f5
ravik993 <ravi.k@mapmyindia.com>
2024-06-30 11:42:03 +0530

    component composition

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
-let Heading =React.createElement('h1',{id:"heading"},'hello react')
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-let parent=React.createElement('div',{id:'parent'},
-    React.createElement('div',{id:'child'},
-   [ React.createElement('h1',{id:'heading'},'i am deep nested h1'),
-    React.createElement('h2',{id:'heading'},'i am deep nested h')
+const titleElement = <h1>hii ravi,how are you..</h1>;
 
-   ]
-))
-let root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
-console.log(parent)
+const Header = ({ title }) => {
+  return (
+    <div>
+      {titleElement}
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+const Footer = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+const Content = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+const Children = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header title="this is title"></Header>
+      <Footer title="this is footer"></Footer>
+      <Content title="this is content"></Content>
+      <children>
+        <h1>My Application</h1>
+        <p>Welcome to my application!</p>
+        <footer>© 2024 My Company</footer>
+        <Header title="this is title"></Header>
+        <Footer title="this is footer"></Footer>
+        <Content title="this is content"></Content>
+      </children>
+    </div>
+  );
+};
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
